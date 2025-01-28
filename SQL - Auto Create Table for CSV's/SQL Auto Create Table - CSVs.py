aac868cb09243eb32351e8ec85bfcc5fb69c89e3
abeyer19 <abeyer22@outlook.com>
2025-01-27 18:26:59 -0600

    Found bug issues for error handling. Updated. Also added in a if statement for checking.

--- SQL - Auto Create Table for CSV's/SQL Auto Create Table - CSVs.py
+++ SQL - Auto Create Table for CSV's/SQL Auto Create Table - CSVs.py
@@ -17,9 +17,12 @@ def dtypes_converter(dtype):
 
 
 # Take in a file from specified from the user
-# Solutions for errors during file path input from user in while loop
 while True:
     path_input = input("File Path: ")
+
+    if not path_input:
+        print("No input, exiting program...")
+        break
 
     # Check if the path has quotes
     if (path_input[0] == "'") or (path_input[-1] == "'"):
@@ -37,8 +40,11 @@ while True:
         print("\nFile path is valid! \nAccessing File.")
         break
 
-# TODO: Figure out why IndexError shows up when I press escape to exit the program
 # Error handling
+    except KeyboardInterrupt:
+        print("\nProgram interrupted by user. Exiting...")
+        break
+
     except pd.errors.ParserError as e:
         print(f"ParserError: There was an issue parsing the CSV file. {e}")
 
@@ -52,23 +58,30 @@ while True:
         print(f"Type Error: {e}")
 
     except PermissionError as e:
-        print(f"Permission Error {e}")
+        print(f"Permission Error: {e}")
 
     except KeyboardInterrupt:
         print("Program interrupted by user. Exiting...")
         break
 
+    except NameError as e:
+        print(f"Name Error: {e}")
+        break
 
-# get file name
-file_name = os.path.basename(path_input)
-file_name = file_name.removesuffix(".csv").lower()
 
-# print out the prompt for pgAdmin4
-print(f"\nDROP TABLE IF EXISTS public.{file_name};\n")
-print(f"CREATE TABLE IF NOT EXISTS public.{file_name}")
-print("(")
-for col in df.columns:
-    print(f'\t{col} {dtypes_converter(df[col].dtypes).lower()} COLLATE pg_catalog."defult"')
-print(");\n")
-print(f"ALTER TABLE IF EXISTS public.{file_name}")
-print("\tOWNER to postgres;")
+# Get file name and print output correctly
+if 'df' in locals():  # Check if df is defined before using it
+    file_name = os.path.basename(path_input)
+    file_name = file_name.removesuffix(".csv").lower()
+
+    # Print out the prompt for pgAdmin4
+    print(f"\nDROP TABLE IF EXISTS public.{file_name};\n")
+    print(f"CREATE TABLE IF NOT EXISTS public.{file_name}")
+    print("(")
+    for col in df.columns:
+        print(f'\t{col} {dtypes_converter(df[col].dtypes).lower()} COLLATE pg_catalog."defult"')
+    print(");\n")
+    print(f"ALTER TABLE IF EXISTS public.{file_name}")
+    print("\tOWNER to postgres;")
+else:
+    print("DataFrame 'df' not created. Exiting.")
