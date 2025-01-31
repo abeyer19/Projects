1bf098b9d1f5a06d14d9f4aed1b377923e7b824e
abeyer19 <abeyer22@outlook.com>
2025-01-30 20:43:45 -0600

    Set up separate files and made some changes to graphs, including histogram log scale on Y axis.

--- Space/Meteorite Landings & Predictions/main.py
+++ Space/Meteorite Landings & Predictions/main.py
@@ -7,6 +7,9 @@ import plotly.express as px
 from matplotlib import pyplot as plt
 import seaborn as sns
 
+# Import from other .py files
+from meteorite_graphs import meteorite_graphs
+
 
 # --- Testing / Tinkering ---
 # Import dataset
@@ -15,49 +18,32 @@ df = pd.read_csv('https://raw.githubusercontent.com/abeyer19/Projects/refs/heads
 print(df['mass (g)'].max())
 
 
-# --- Real Analysis ---
-# Replace nan's with 0
-df['mass (g)'] = df['mass (g)'].fillna(0)
-# Add column to convert mass (g) to mass (lbs)
-df['mass (lbs)'] = round(df['mass (g)'] * 0.00220462262, 2)
-# Change columns to numeric
-df['mass (g)'] = pd.to_numeric(df['mass (g)'])
-df['mass (lbs)'] = pd.to_numeric(df['mass (lbs)'])
+    # --- Real Analysis ---
+def main():
+    # Replace nan's with 0
+    df['mass (g)'] = df['mass (g)'].fillna(0)
+    # Add column to convert mass (g) to mass (lbs)
+    df['mass (lbs)'] = round(df['mass (g)'] * 0.00220462262, 2)
+    # Change columns to numeric
+    df['mass (g)'] = pd.to_numeric(df['mass (g)'])
+    df['mass (lbs)'] = pd.to_numeric(df['mass (lbs)'])
 
 
-# Understand statistics
-    # 1. Basic statistics
-print(df.describe())
-    # 2. Histogram of sizes
-sns.histplot(df, 
-             x='mass (lbs)', 
-             bins=100, 
-             binrange=(-2.5,df['mass (lbs)'].mean()), 
-             stat='probability',)
-plt.show()
+    # Understand statistics
+        # 1. Basic statistics
+        # 2. Histogram of sizes
+        # TODO: 3. Bar charts of nametypes
+    # Visual current data on geomap of globe using reclat and reclong
+        # 1. Interactive map showing each point on the map
+        # 2. Increase in either size or decrease color oppacity for mass
+        # 3. Create seperate scatter_geo for each continent
+    meteorite_graphs()
 
-sns.histplot(df, 
-             x='mass (lbs)', 
-             bins=100, 
-             binrange=(1000,df['mass (lbs)'].max()), 
-             stat='probability',)             
+    # Create simulations based on location and mass of meteorites
+        # 1. Where are the biggest ones going to hit?
+        # 2. Where is the most highly condensed area where they will hit
+        # 3. Show visualization of simulations in real time (if possible)
 
-plt.show()
-    # 3. Bar charts of nametypes
 
-# Visual current data on geomap of globe using reclat and reclong
-    # 1. Interactive map showing each point on the map
-    # 2. Increase in either size or decrease color oppacity for mass
-fig = px.scatter_geo(df, 
-                     lat="reclat", 
-                     lon="reclong",
-                     #animation_frame="year", 
-                     opacity= 1, 
-                     size= "mass (g)",
-                     projection="natural earth",)
-fig.show()
-
-# Create simulations based on location and mass of meteorites
-    # 1. Where are the biggest ones going to hit?
-    # 2. Where is the most highly condensed area where they will hit
-    # 3. Show visualization of simulations in real time (if possible)
+if __name__ == "__main__":
+    main()
