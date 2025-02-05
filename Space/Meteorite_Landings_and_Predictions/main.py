4fa1dccfc0d6c035f3bc15d8920671a44b6f7d1c
Andrew Beyer <abeyer22@outlook.com>
2025-02-04 18:16:00 -0600

    updates

--- Space/Meteorite_Landings_and_Predictions/main.py
+++ Space/Meteorite_Landings_and_Predictions/main.py
@@ -6,24 +6,51 @@ import plotly.express as px
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-# Import from other .py files
-from meteorite_graphs import meteorite_graphs
-
 
 # --- Testing / Tinkering ---
 # Import dataset
 df = pd.read_csv('https://raw.githubusercontent.com/abeyer19/Projects/refs/heads/main/Space/Meteorite_Landings_and_Predictions/Meteorite_Landings_20250128.csv')
 
+
 # --- Real Analysis ---
+def meteorite_graphs(df):
+    # Replace nan's with 0
+    df['mass (g)'] = df['mass (g)'].fillna(0)
+    # Add column to convert mass (g) to mass (lbs)
+    df['mass (lbs)'] = round(df['mass (g)'] * 0.00220462262, 2)
+    # Change columns to numeric
+    df['mass (g)'] = pd.to_numeric(df['mass (g)'])
+    df['mass (lbs)'] = pd.to_numeric(df['mass (lbs)'])
+
 # Understand statistics
     # 1. Basic statistics
-    # 2. Histogram of sizes
-    # TODO: 3. Bar charts of nametypes
-# Visual current data on geomap of globe using reclat and reclong
-    # 1. Interactive map showing each point on the map
-    # 2. Increase in either size or decrease color oppacity for mass
-    # 3. Create seperate scatter_geo for each continent
-meteorite_graphs(df)
+    print(df.describe())
+        # 2. Histogram of sizes, y in log scale to show outliers
+    sns.histplot(df, 
+                x='mass (lbs)', 
+                bins=100, 
+                stat='count', 
+                ).set_yscale('log')
+    plt.show()
+        # 3. TODO: Bar charts of nametypes
+
+
+    # Visual current data on geomap of globe using reclat and reclong
+        # 1. Interactive map showing each point on the map
+            # a. Increase in either size or decrease color oppacity for mass
+    fig = px.scatter_geo(df, 
+                        lat="reclat", 
+                        lon="reclong",
+                        #animation_frame="year", 
+                        opacity= 1, 
+                        size= "mass (g)",
+                        projection="natural earth",
+                        hover_data=["mass (g)", "recclass", "year"]).update_geos(showcoastlines=True, 
+                                    coastlinecolor="black",
+                                    showland=True, 
+                                    landcolor="black",)
+    fig.show()
+        # 3. TODO: Create seperate scatter_geo for each continent
 
 # Create simulations based on location and mass of meteorites
     # 1. Where are the biggest ones going to hit?
