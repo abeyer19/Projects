565b4998d09d39693319f6924dd0d8b0630b3df1
Andrew Beyer <abeyer22@outlook.com>
2025-02-04 20:11:07 -0600

    Added in Geo Location Groupings and looked at counts of the different groupings. Total doesn't match the count of the lat/long columns in dataset, definitely missing some data points but will move on.

--- Space/Meteorite_Landings_and_Predictions/main.py
+++ Space/Meteorite_Landings_and_Predictions/main.py
@@ -9,7 +9,7 @@ import seaborn as sns
 
 # --- Testing / Tinkering ---
 # Import dataset
-df = pd.read_csv('https://raw.githubusercontent.com/abeyer19/Projects/refs/heads/main/Space/Meteorite_Landings_and_Predictions/Meteorite_Landings_20250128.csv')
+meteors = pd.read_csv('https://raw.githubusercontent.com/abeyer19/Projects/refs/heads/main/Space/Meteorite_Landings_and_Predictions/Meteorite_Landings_20250128.csv')
 
 
 # --- Real Analysis ---
@@ -52,7 +52,62 @@ def meteorite_graphs(df):
     fig.show()
         # 3. TODO: Create seperate scatter_geo for each continent
 
+# Show meteorite_graphs
+#meteorite_graphs(meteors)
+
+
+# --- Predictions ---
 # Create simulations based on location and mass of meteorites
+
+# Geo Locations Groupings
+# North America
+n_america_lat = (25, 70)
+n_america_long = (-170, -50)
+
+# South America
+s_america_lat = (-55, 25)
+s_america_long = (-80, -35)
+
+# Europe
+euro_lat = (35, 70)
+euro_long = (-10, 40)
+
+# Africa
+africa_lat = (-35, 35)
+africa_long = (-17, 50)
+
+# Asia
+asia_lat = (25, 70)
+asia_long = (40, 180)
+
+# Australia
+australia_lat = (-55, -10)
+australia_long = (110, 180)
+
+# Artic
+artic_lat = (70, 90)
+artic_long = (-180, 180)
+
+# Counts of meteorites in each region
+n_america_count = meteors[(meteors['reclat'] >= n_america_lat[0]) & (meteors['reclat'] <= n_america_lat[1]) & (meteors['reclong'] >= n_america_long[0]) & (meteors['reclong'] <= n_america_long[1])].shape[0]
+s_america_count = meteors[(meteors['reclat'] >= s_america_lat[0]) & (meteors['reclat'] <= s_america_lat[1]) & (meteors['reclong'] >= s_america_long[0]) & (meteors['reclong'] <= s_america_long[1])].shape[0]
+euro_count = meteors[(meteors['reclat'] >= euro_lat[0]) & (meteors['reclat'] <= euro_lat[1]) & (meteors['reclong'] >= euro_long[0]) & (meteors['reclong'] <= euro_long[1])].shape[0]
+africa_count = meteors[(meteors['reclat'] >= africa_lat[0]) & (meteors['reclat'] <= africa_lat[1]) & (meteors['reclong'] >= africa_long[0]) & (meteors['reclong'] <= africa_long[1])].shape[0]
+asia_count = meteors[(meteors['reclat'] >= asia_lat[0]) & (meteors['reclat'] <= asia_lat[1]) & (meteors['reclong'] >= asia_long[0]) & (meteors['reclong'] <= asia_long[1])].shape[0]
+australia_count = meteors[(meteors['reclat'] >= australia_lat[0]) & (meteors['reclat'] <= australia_lat[1]) & (meteors['reclong'] >= australia_long[0]) & (meteors['reclong'] <= australia_long[1])].shape[0]
+artic_count = meteors[(meteors['reclat'] >= artic_lat[0]) & (meteors['reclat'] <= artic_lat[1]) & (meteors['reclong'] >= artic_long[0]) & (meteors['reclong'] <= artic_long[1])].shape[0]
+
+print(meteors.count())
+print(n_america_count)
+print(s_america_count)
+print(euro_count)
+print(africa_count)
+print(asia_count)
+print(australia_count)
+print(artic_count)
+print(n_america_count + s_america_count + euro_count + africa_count + asia_count + australia_count + artic_count)
+
+
     # 1. Where are the biggest ones going to hit?
     # 2. Where is the most highly condensed area where they will hit
     # 3. Show visualization of simulations in real time (if possible)
