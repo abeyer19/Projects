156b5b17dac49036ad024a886ed5ae6af0371342
Andrew Beyer <abeyer22@outlook.com>
2025-06-18 17:58:44 -0500

    was able to update pully.py to be way more dynamic based on whatever table is given to the function.

--- ETL/YouTube/Lifters/pully.py
+++ ETL/YouTube/Lifters/pully.py
@@ -4,12 +4,6 @@ import sys
 from dotenv import load_dotenv
 import os
 
-
-# SUDO-CODE:
-# -- pull data from database -> specified by items in config.env
-# -- return as a pandas df for easy manipulation
-# -- 
-
 # Load in config.env file for Database connection secrets
 load_dotenv("config.env")
 host = os.getenv("host")
@@ -17,29 +11,29 @@ port:int = os.getenv("port")
 user = os.getenv("user")
 dbname1 = os.getenv("dbname1")
 
-def pully(host:str, port:int, user:str, dbname1:str, table:str=None, query_limit:int=None):
-    with psycopg.connect(host=host, port=port, user=user, dbname=dbname1) as conn_dbs1:
-        with conn_dbs1.cursor() as cur:
-    # -- take all arguments and select * from said table -> allowing limit of return values
-            cur.execute("SELECT * FROM %s LIMIT %s" % (table, query_limit))
-            videos = cur.fetchall()
-            video_ids, video_titles, publication_dates, view_counts, like_counts, comment_counts, video_durations, channel_ids, category_ids, record_dates = zip(*videos)
+def pully(host:str, port:int, user:str, dbname:str, table:str, limit:int=None):
+    with psycopg.connect(host=host, port=port, user=user, dbname=dbname) as dbs1:
+        with dbs1.cursor() as cur:
+            cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = %s ORDER BY ordinal_position;", (table,))
+            response_columns = cur.fetchall()
 
-            video_df = pd.DataFrame({
-                'video_id': video_ids,
-                'video_title': video_titles,
-                'publication_date': publication_dates,
-                'view_count': view_counts,
-                'like_count': like_counts,
-                'comment_count': comment_counts,
-                'video_duration': video_durations,
-                'channel_id': channel_ids,
-                'category_id': category_ids,
-                'record_date': record_dates,
-            })
+            table_columns = []
 
-            return video_df
+            for record in response_columns:
+                table_columns.append(record[0])
+            
+            if limit is None:
+                cur.execute("SELECT * FROM {}".format(table))
+                response_data = cur.fetchall()
+            else:
+                cur.execute("SELECT * FROM {} LIMIT {}".format(table, limit))
+                response_data = cur.fetchall()
 
-        conn_dbs1.close()
+            df = pd.DataFrame(data=response_data, columns=table_columns)
 
-print(pully(host=host, port=port, user=user, dbname1=dbname1, table="videos", query_limit=2))
+            cur.close()
+
+            return df
+
+# Testing only -- delete after finalization
+print(pully(host=host, port=port, user=user, dbname=dbname1, table='videos'))
