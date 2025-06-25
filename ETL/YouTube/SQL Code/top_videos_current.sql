946adb0f8b72f17918e0fa20e4cd6dbdeb76336d
Andrew Beyer <abeyer22@outlook.com>
2025-06-25 18:59:16 -0500

    Update top_videos_current.sql
    added code

--- ETL/YouTube/SQL Code/top_videos_current.sql
+++ ETL/YouTube/SQL Code/top_videos_current.sql
@@ -1,1 +1,29 @@
-
+WITH videos_channels AS (
+SELECT videos.video_id,
+            videos.video_title,
+            videos.view_count,
+            videos.like_count,
+            videos.comment_count,
+            videos.category_id,
+            categories.category_title,
+            videos.record_date,
+            videos.channel_id AS video_channel_id,
+            channels.channel_id AS channel_channel_id,
+            channels.channel_name
+           FROM videos
+             LEFT JOIN channels ON videos.channel_id = channels.channel_id
+             LEFT JOIN categories ON videos.category_id = categories.category_id
+        )
+ SELECT DISTINCT ON (videos_channels.video_id) videos_channels.video_id,
+    videos_channels.video_title,
+    videos_channels.view_count,
+    videos_channels.like_count,
+    videos_channels.comment_count,
+    videos_channels.view_count + videos_channels.like_count + videos_channels.comment_count AS total_engagement,
+    videos_channels.video_channel_id AS channel_id,
+    videos_channels.channel_name,
+    videos_channels.category_id,
+    videos_channels.category_title,
+    videos_channels.record_date
+   FROM videos_channels
+  ORDER BY videos_channels.video_id, videos_channels.record_date DESC;
