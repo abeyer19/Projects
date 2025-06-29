2b6c398a75af291c270e273e8790b292bf45089a
Andrew Beyer <abeyer22@outlook.com>
2025-06-28 21:23:02 -0500

    fixed search() to take a random single string, or letter, to pass as the query argument if none given. -- closest I could get to a random search as possible

--- ETL/YouTube/Scrapers/search.py
+++ ETL/YouTube/Scrapers/search.py
@@ -1,6 +1,8 @@
 # Importy all packages
 import pandas as pd
 from api_connection import youtube
+import random
+import string
 
 # Search is only to be used for basic searching of channel, video, and playlist metadata such as ID and Titles; all other statistics to be collected through collector.py
 def search(result_limit:int, type:str, query:str=None, video_category_id:int=None, video_duration:str=None):
@@ -28,6 +30,32 @@ def search(result_limit:int, type:str, query:str=None, video_category_id:int=Non
         return channel_data
 
     # To be used only if 'type' = 'video'
+    elif type == 'video' and query is None:
+
+        rand_query = random.choice(string.ascii_lowercase)
+
+        request = youtube.search().list(
+            part='id',
+            maxResults=result_limit,
+            regionCode='US',
+            type=type,
+            q=rand_query,
+            videoCategoryId=video_category_id,
+            videoDuration=video_duration,)
+        
+        response = request.execute()
+        
+        video_ids = []
+
+        for item in response['items']:
+            video_ids.append(item['id']['videoId'])
+
+        video_data = pd.DataFrame({
+            'video_id': video_ids,
+        })
+
+        return video_data
+        
     elif type == 'video':
         request = youtube.search().list(
             part='id',
