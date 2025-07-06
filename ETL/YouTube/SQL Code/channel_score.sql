65ba0202f4bbfe108a0668bb74bbc895def4a782
Andrew Beyer <abeyer22@outlook.com>
2025-07-05 19:33:58 -0500

    Update channel_score.sql
    needed to update to do the same as video dashboard

--- ETL/YouTube/SQL Code/channel_score.sql
+++ ETL/YouTube/SQL Code/channel_score.sql
@@ -1,7 +1,7 @@
-SELECT c.channel_id, 
-	c.channel_name, 
-	(c.channel_view_count/c.video_count) AS view_per_video, 
-	CAST (AVG(c.channel_view_count/c.video_count) OVER (PARTITION BY c.channel_id) AS BIGINT) AS avg_view_per_video, 
-	c.record_date
-FROM channels as c
-ORDER BY c.channel_id DESC;
+ SELECT DISTINCT ON (c.channel_id) c.channel_id,
+    c.channel_name,
+    c.channel_view_count / c.video_count AS view_per_video,
+    avg(c.channel_view_count / c.video_count) OVER (PARTITION BY c.channel_id)::bigint AS avg_view_per_video,
+    c.record_date
+   FROM channels c
+  ORDER BY c.channel_id, c.record_date DESC;
