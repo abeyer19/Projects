426d1055c334e5c9d3d1b7624b17ef58beaee57c
Andrew Beyer <abeyer22@outlook.com>
2025-12-12 14:07:51 -0600

    Add SUDO code comments to scraper.py
    Added SUDO code comments outlining the scraping process.

--- Atlas of Sound/Main/Scrapers/scraper.py
+++ Atlas of Sound/Main/Scrapers/scraper.py
@@ -1,1 +1,7 @@
+#SUDO Code 
 
+# 1. use Requests packaged to access https://www.whosampled.com/
+    # https://www.whosampled.com/sitemap/artist/A/ - example
+# 2. specify URLs and length of pages to pull in pull function
+# 3. extract data needed and transform this into a pandas df
+#  a. get: artist, album name, song name, album year, sampled song(s) name, sampled song artist, sampled song album, sampled song album year
