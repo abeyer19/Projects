45a0649bf312ad131cbfe9201845204840d0bb97
Andrew <abeyer22@outlook.com>
2025-12-16 18:29:21 -0600

    more basic preperations and testing of requests

--- Atlas of Sound/Main/Scrape/scraper.py
+++ Atlas of Sound/Main/Scrape/scraper.py
@@ -5,3 +5,13 @@
 # 2. specify URLs and length of pages to pull in pull function
 # 3. extract data needed and transform this into a pandas df
 #  a. get: artist, album name, song name, album year, sampled song(s) name, sampled song artist, sampled song album, sampled song album year
+
+import requests
+
+url = "https://jsonplaceholder.typicode.com/posts/1"
+response = requests.get(url)
+
+print(response.status_code)  # HTTP status code (200 = OK)
+print(response.text)
+data = response.json()
+print(data["title"])
